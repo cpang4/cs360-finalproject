d87704e78499f542ff5f2bdf6edbe4f077d903b6
Claire Pang <31524697+cpang4@users.noreply.github.com>
2018-04-18 20:59:48 -0700

    Update yearCountProcessing.R

--- code/yearCountProcessing.R
+++ code/yearCountProcessing.R
@@ -1,5 +1,3 @@
-# Code used to create the "skyscrapers-count.csv", which counts the number of skyscrapers existing in the US for a year.
-
 setwd("C:/Users/ciz4c/Desktop/final")
 data <- read.csv("skyscrapers.csv")
 
@@ -15,6 +13,7 @@ temp.df <- cbind(temp.df, vect)
 colnames(temp.df) <- c("year", "num completed", "cumulative")
 
 df <- as.data.frame(1912:2016)
+colnames(df) <- c("year")
 tempvec <- c()
 for (i in 1:nrow(df)){
   if (df$year[i]%in%temp.df$year){
@@ -25,6 +24,87 @@ for (i in 1:nrow(df)){
   }
 }
 df <- cbind(df, tempvec)
-colnames(df) <- c("year", "cumulative")
 
+# by purpose
+hotels <- which(data$hotel == "TRUE")
+hotelRows <- data[hotels,]
+
+temp.df <- as.data.frame(table(hotelRows$completed.year))
+count <- 0
+vect <- c()
+for (i in 1:nrow(temp.df)) {
+  count <- count + temp.df$Freq[i]
+  vect <- c(vect, count)
+}
+
+temp.df <- cbind(temp.df, vect)
+colnames(temp.df) <- c("year", "num completed", "cumulative")
+
+tempvec <- c()
+for (i in 1:nrow(df)){
+  if (df$year[i]%in%temp.df$year){
+    tempvec <- c(tempvec, temp.df$cumulative[which(temp.df$year == df$year[i])])
+  }
+  else if (df$year[i] == 1912){
+    tempvec <- c(tempvec, 0)
+  }
+  else  {tempvec <- c(tempvec, tempvec[i-1])
+  }
+}
+df <- cbind(df, tempvec)
+
+
+# offices
+office <- which(data$office == "TRUE")
+officeRows <- data[office,]
+temp.df <- as.data.frame(table(officeRows$completed.year))
+count <- 0
+vect <- c()
+for (i in 1:nrow(temp.df)) {
+  count <- count + temp.df$Freq[i]
+  vect <- c(vect, count)
+}
+
+temp.df <- cbind(temp.df, vect)
+colnames(temp.df) <- c("year", "num completed", "cumulative")
+tempvec <- c()
+for (i in 1:nrow(df)){
+  if (df$year[i]%in%temp.df$year){
+    tempvec <- c(tempvec, temp.df$cumulative[which(temp.df$year == df$year[i])])
+  }
+  else if (df$year[i] == 1912){
+    tempvec <- c(tempvec, 0)
+  }
+  else  {tempvec <- c(tempvec, tempvec[i-1])
+  }
+}
+df <- cbind(df, tempvec)
+
+# residential
+residential <- which(data$residential == "TRUE")
+resRows <- data[residential,]
+temp.df <- as.data.frame(table(resRows$completed.year))
+count <- 0
+vect <- c()
+for (i in 1:nrow(temp.df)) {
+  count <- count + temp.df$Freq[i]
+  vect <- c(vect, count)
+}
+
+temp.df <- cbind(temp.df, vect)
+colnames(temp.df) <- c("year", "num completed", "cumulative")
+tempvec <- c()
+for (i in 1:nrow(df)){
+  if (df$year[i]%in%temp.df$year){
+    tempvec <- c(tempvec, temp.df$cumulative[which(temp.df$year == df$year[i])])
+  }
+  else if (df$year[i] == 1912){
+    tempvec <- c(tempvec, 0)
+  }
+  else  {tempvec <- c(tempvec, tempvec[i-1])
+  }
+}
+df <- cbind(df, tempvec)
+colnames(df) <- c("year", "cumulative", "hotel", "office", "residential")
 write.csv(df, file="skyscrapers-count.csv")
+
