feb724891315434c260b8b520504bf2453e16320
Claire Pang <31524697+cpang4@users.noreply.github.com>
2018-05-08 10:57:03 -0700

    Update yearCountProcessing.R

--- code/yearCountProcessing.R
+++ code/yearCountProcessing.R
@@ -1,3 +1,5 @@
+### GENERATES skyscrapers-count.csv
+### Counts cumlatively how many skyscrapers are completed by year, and for each of the main purposes (hotel, residential, office, and other)
 data <- read.csv("skyscrapers-main.csv")
 
 temp.df <- as.data.frame(table(data$completed.year))
@@ -141,3 +143,36 @@ for (i in 1:nrow(df)){
 df <- cbind(df, tempvec)
 colnames(df) <- c("year", "all", "hotel", "office", "residential", "other")
 write.csv(df, file="skyscrapers-count.csv")
+
+### GENERATES skyscrapers-top10.csv
+### Gets Top 10 Tallest for each city/overall, for a city that has > 10 skyscrapers
+
+cities <- c("All", "Atlanta", "Boston", "Chicago", "Dallas", "Detroit", "Houston", "Jersey City", "Las Vegas", "Los Angeles", "Miami", "New York City", "Philadelphia", "Pittsburgh", "San Francisco", "Seattle")
+cityDF <- data.frame()
+
+for (city in 1:length(cities)){
+
+  if (cities[city] == "All"){
+    temp <-data.frame()
+    for (i in 1:10){
+      temp <- rbind(temp, data[i,])
+    }
+    cityDF <- rbind(cityDF, temp)
+  }
+  else {
+    temp <- data.frame()
+    cityInd <- which(data$city == cities[city])
+    for (i in 1:10){
+      temp <- rbind(temp, data[cityInd[i],])
+    }
+    cityDF <- rbind(cityDF, temp)
+  }
+}
+category <- c()
+for (city in 1:length(cities)){
+  for (i in 1:10){
+    category <- c(category, cities[city]) 
+  }
+}
+cityDF <- cbind(category, cityDF)
+write.csv(cityDF, file="skyscrapers-top10.csv")
